555d929bd3d2ba6e3c4dba1a29f7dd4683c6e4eb
Sneha Govindarajan <govindarajan.sn@northeastern.edu>
2023-02-07 16:12:52 -0500

    initial commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -225,52 +225,6 @@
       initSwipeContent(this);
     };
     
-    // --
-
-    var span = document.querySelector(".typing-container .container .typewriter span");
-    var textArr = span.getAttribute("data-text").split(", "); 
-    var maxTextIndex = textArr.length; 
-
-var sPerChar = 0.15; 
-var sBetweenWord = 1.5;
-var textIndex = 0; 
-
-typing(textIndex, textArr[textIndex]); 
-
-function typing(textIndex, text) {
-  console.log("hello");
-    var charIndex = 0; 
-    var maxCharIndex = text.length - 1; 
-    
-    var typeInterval = setInterval(function () {
-        span.innerHTML += text[charIndex]; 
-        if (charIndex == maxCharIndex) {
-            clearInterval(typeInterval);
-            setTimeout(function() { deleting(textIndex, text) }, sBetweenWord * 1000); 
-            
-        } else {
-            charIndex += 1; 
-        }
-    }, sPerChar * 1000); 
-}
-
-function deleting(textIndex, text) {
-    var minCharIndex = 0; 
-    var charIndex = text.length - 1; 
-
-    var typeInterval = setInterval(function () {
-        span.innerHTML = text.substr(0, charIndex); 
-        if (charIndex == minCharIndex) {
-            clearInterval(typeInterval);
-            textIndex + 1 == maxTextIndex ? textIndex = 0 : textIndex += 1; 
-            setTimeout(function() { typing(textIndex, textArr[textIndex]) }, sBetweenWord * 1000); 
-        } else {
-            charIndex -= 1; 
-        }
-    }, sPerChar * 1000); 
-}
-
-// --
   
     function initSwipeContent(content) {
       content.element.addEventListener('mousedown', handleEvent.bind(content));
